78b984e6aefef094316628c8395d1832d931d1b5
Laxman <lakshmanteja9@gmail.com>
2025-01-08 13:18:46 +0530

    added_liabraries&setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="Passenger-Referral-Prediction",
+      version="0.0.0",
+      author="Laxman",
+      author_email="lakshmanteja9@gmail.com",
+      description="IndiGo-Airline-Passenger-Referral-Prediction",
+      packages=find_packages()
+
+)
